c4d6e826a8722c7a0f036742c17195df39da9cf4
Maria <marikoreneva@gmail.com>
2017-12-15 01:58:23 +0300

    Added comments

--- test/TestAnalyticsService.py
+++ test/TestAnalyticsService.py
@@ -4,7 +4,7 @@ from src.AnalyticsService import AnalyticsService
 
 class TestAnalyticsService(unittest.TestCase):
     vk_acces_token = "cf6545c36443a4e988ce182452e968be64f45354507ee2395b9adab4ce95b653d3ab962b9160221498ed0"
-    fb_acces_token = "EAACEdEose0cBAFErsuXLFpAvtOSPf5S3ORGJ9iqu6ez6jN3OoIcBxgvEQ1cG2bVwRZCitiEbADT0Ty9pR7WpmZBuFuDCQZCk4wNxDWZCDhYF48ZBR3PhTdu2jAt9zux5dqvBQt1f7JAlWJu9OzmnAUaa8fDtN8iUy6lxkFrMEVTkr4UcZC1ysewaE3ZAZAoo7QuAPuAtSFBQ1wZDZD"
+    fb_acces_token = "EAACEdEose0cBAMT9wH6gn9F2EurjNJ3AmQckTNtV9g3pJHL92ekxiWD3ricpNNyJzquXToIzkBSef1up1QrU2bE5tZBH2quYu0uXIdZA3D5tZC04e8ZCLpg3HxalvZBS6ZAipvxZC8Lq5U1ObDpT4RNZCGlQVs45ZBqGXW8KbywZBNTIoUYVInteeU2hRPLa5OPwIZD"
     analytics = AnalyticsService()
     
     # get_user_info
@@ -101,3 +101,61 @@ class TestAnalyticsService(unittest.TestCase):
         ]
         meetings = self.analytics.get_meetings('fb', self.fb_acces_token, 'en', people)
         self.assertTrue(len(meetings) > 0)
+
+    def test_get_meetings_vk_ru(self):
+        people = [
+            {
+                'id': 0,
+                'job_position': 'software developer',
+                'social_network': {
+                    'name': 'fb',
+                    'id': '906351386055094'
+                }
+            }, {
+                'id': 1,
+                'job_position': 'software developer',
+                'social_network': {
+                    'name': 'vk',
+                    'id': '17611433'
+                }
+            }, {
+                'id': 3,
+                'job_position': 'software developer',
+                'social_network': {
+                    'name': 'vk',
+                    'id': '215524314'
+                }
+            }
+        ]
+        meetings = self.analytics.get_meetings('vk', self.vk_acces_token, 'ru', people)
+        self.assertTrue(len(meetings) > 0)
+        print(meetings)
+
+    def test_get_meetings_fb_ru(self):
+        people = [
+            {
+                'id': 0,
+                'job_position': 'software developer',
+                'social_network': {
+                    'name': 'fb',
+                    'id': 'me'
+                }
+            }, {
+                'id': 1,
+                'job_position': 'бухгалтер',
+                'social_network': {
+                    'name': 'vk',
+                    'id': '17611433'
+                }
+            }, {
+                'id': 3,
+                'job_position': 'artist',
+                'social_network': {
+                    'name': 'fb',
+                    'id': '10155138239048996'
+                }
+            }
+        ]
+        meetings = self.analytics.get_meetings('fb', self.fb_acces_token, 'ru', people)
+        self.assertTrue(len(meetings) > 0)
+        print(meetings)
